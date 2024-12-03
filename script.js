e676060168c9b13c4e9b6b0c6f7abd79c4453f6d
malxcdev <malxcondev@gmail.com>
2024-12-03 06:36:13 -0500

    90%

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let containerSocial = document.querySelector('#container-social');
-let containerAvatar = document.querySelector('#container-social');
+let containerAvatar = document.querySelector('#container-avatar');
 
 let btnSocial = document.querySelector('#btn-social');
 let btnAvatar = document.querySelector('#btn-avatar');
 
 
 btnAvatar.addEventListener('click', () => {
-    containerAvatar.classList.toggle('hidden');
+    containerAvatar.classList.add('hidden');
     containerSocial.classList.remove('hidden');
+});
+
+btnAvatar.addEventListener('mouseover', () => {
+    containerSocial.classList.remove('md:hidden');
 });
